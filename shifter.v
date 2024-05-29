06e6512c0c8c3eb423e5f8f3321b0f583ba161b9
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-05-29 16:31:57 +0530

    Update shifter.v

--- shifter.v
+++ shifter.v
@@ -1,1 +1,26 @@
-module 
+module var_shift #(parameter N = 32) ( input [N-1:0]a,
+                                      input clk , dir , clr , en ,
+                                      output reg[N-1:0] q ) 
+  always @ (posedge clk)  
+      if(!clr)
+        q <= 0;
+      else 
+        begin 
+          if(en)
+            case (dir)
+              0 : { 0 , q[N-1:1]};
+              1 : { q[N-2:0] , 0};
+            endcase
+          else 
+            q <= q ; 
+        end 
+endmodule
+
+                
+              
+              
+        
+  
+  
+
+  
