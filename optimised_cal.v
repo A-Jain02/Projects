b0e9128fe9a09ea5770b5a22e1f483861cf2de88
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-05-29 11:54:51 +0530

    Update optimised_cal.v

--- optimised_cal.v
+++ optimised_cal.v
@@ -1,10 +1,57 @@
 module calculator (a,b,oper, out);
   input [3:0] a , b;
-  input [1:0] oper;
+  input [2:0] oper;
   output reg [7:0] out; 
-  output reg [7:0] sum, diff , prod , div , rem ;
+  output reg [7:0] sum, diff , prod , div , mod ;
 
-  assign out = sum | diff | prod | div ;
+  assign out = sum | diff | prod | div | mod ;
 
-  case (oper)
+  always @ (*)
+    begin
+      case (oper) 
+    
+        3'b000 : begin 
+          sum <= a + b ; 
+          diff <= 7'b0 ;
+          prod <= 7'b0 ; 
+          div <= 7'b0 ; 
+          mod <= 7'b0 ; 
+        end
+
+        3'b001 : begin 
+          sum <= 7'b0 ; 
+          diff <= a-b ;
+          prod <= 7'b0 ; 
+          div <= 7'b0 ; 
+          mod <= 7'b0 ; 
+        end
+
+        3'b010 : begin 
+          sum <= 7'b0 ; 
+          diff <= 7'b0 ;
+          prod <= a * b ; 
+          div <= 7'b0 ; 
+          mod <= 7'b0 ; 
+        end
+
+        3'b011 : begin 
+          sum <= 7'b0 ; 
+          diff <= 7'b0 ;
+          prod <= 7'b0 ; 
+          div <= a / b ; 
+          mod <= 7'b0 ; 
+        end
+
+        3'b100 : begin 
+          sum <= 7'b0 ; 
+          diff <= 7'b0 ;
+          prod <= 7'b0 ; 
+          div <= 7'b0 ; 
+          mod <= a % b ; 
+        end
+
+        default :
+          
+        
+          
   
