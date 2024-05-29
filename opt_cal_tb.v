428c62b240a5ea015ab1d91f1a4e1f3a32a2bf4e
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-05-29 12:23:04 +0530

    Update opt_cal_tb.v

--- opt_cal_tb.v
+++ opt_cal_tb.v
@@ -1,12 +1,18 @@
 `timescale 1ns / 1ps
 
-module TC_calculator ; 
+module calculator_tst ; 
   reg [3:0] a ;
   reg [3:0] b ;  
   reg [2:0] oper ; 
 
   wire [7:0] out; 
-  
+
+  calculator uut ( 
+    .a(a) ,
+    .b(b) ,
+    .oper(oper) ,
+    .out(out) ,
+  );
 
   
   
