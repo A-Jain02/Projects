6367f5342e4e371b513f5fe3148beef9e95dfc0b
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-05-29 11:49:38 +0530

    Update optimised_cal.v

--- optimised_cal.v
+++ optimised_cal.v
@@ -1,1 +1,10 @@
+module calculator (a,b,oper, out);
+  input [3:0] a , b;
+  input [1:0] oper;
+  output reg [7:0] out; 
+  output reg [7:0] sum, diff , prod , div , rem ;
 
+  assign out = sum | diff | prod | div ;
+
+  case (oper)
+  
