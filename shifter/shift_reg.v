de6befc3989d8f401cf5bb0882cc8b766f86ec7c
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-05-30 09:08:47 +0530

    Update shift_reg.v

--- shifter/shift_reg.v
+++ shifter/shift_reg.v
@@ -1,16 +1,16 @@
 module var_shift #(parameter N = 32) (input clk , dir , clr , output reg[N-1:0] q) 
   
   always @ (posedge clk)  
-      if(!clr)
+    if(!clr) // clear pin is active low always
         q <= 0;  // if clear then output resets
       else
         begin
-          if(en)
+          if(en) // output enable
             case (dir)
-              0 : { 0 , q[N-1:1]};  // right shift
-              1 : { q[N-2:0] , 0};  // left shift
+              0 : { 0 , q[N-1:1] };  // right shift
+              1 : { q[N-2:0] , 0 };  // left shift
             endcase
-          else
+          else  // output disable
             q <= q ;
         end 
 endmodule
