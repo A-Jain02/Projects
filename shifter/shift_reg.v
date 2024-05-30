3cad3520053dd72714d76862bb873ccebcac86cd
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-05-30 09:05:44 +0530

    Update shift_reg.v

--- shifter/shift_reg.v
+++ shifter/shift_reg.v
@@ -3,12 +3,15 @@ module var_shift #(parameter N = 32) (input clk , dir , clr , output reg[N-1:0] 
   always @ (posedge clk)  
       if(!clr)
         q <= 0;  // if clear then output resets
-      else 
+      else
         begin
+          if(en)
             case (dir)
               0 : { 0 , q[N-1:1]};  // right shift
               1 : { q[N-2:0] , 0};  // left shift
             endcase
+          else
+            q <= q ;
         end 
 endmodule
 
