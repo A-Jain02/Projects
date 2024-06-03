2829a43fbd7ebbbf6c68d57c17304902c3a4536f
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-06-03 10:06:47 +0530

    Update mod_m_counter.sv

--- Del_Ded/mod_m_counter.sv
+++ Del_Ded/mod_m_counter.sv
@@ -6,6 +6,28 @@ module mod_m_counter
     output logic max_tick;
   );
 
-  local parameter N = $clog2(M);
+  localparam N = $clog2(M);
+
+  logic [N-1:0] r_reg , r_next ;
+
+  always_ff @( posedge clk , posedge reset )
+    begin 
+      if(reset)
+        r_reg <= 0;
+      else 
+        r_reg <= r_next;
+    end
+
+  assign r_next = ( r_reg == (M - 1)) ? 0 : r_reg + 1 ;
+
+  assign q = r_reg ;
+
+  assign max_tick = ( r_reg == (M - 1)) ? 1'b1 : 1'b0;
+endmodule 
+
+  
+    
+
+  
   
   
