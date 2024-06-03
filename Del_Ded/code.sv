d3ab4c0ec27a53baf02f266eb8231f18e36089d1
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-06-03 10:47:56 +0530

    Update code.sv

--- Del_Ded/code.sv
+++ Del_Ded/code.sv
@@ -4,6 +4,7 @@ module delayed_debouncer (
   input logic reset ;
   output logic db ;
 );
+  
   logic m_tick;
   mod_m_counter #(.M(1_000_000)) ticker (  // (we need 10 ms counter. so 10 ns req 1 mil ticks to get to 10 ms. Hence the 1000000)
                   .clk(clk)
@@ -12,11 +13,12 @@ module delayed_debouncer (
                   max_tick(m_tick)
   );
   
-  typedef enum { zero , wait1_0, wait1_1, wait1_2, one, wait0_0, wait0_1, wait0_2} state_type ;
+  typedef enum { zero , wait1_0, wait1_1, wait1_2, one, wait0_0, wait0_1, wait0_2} state_type ; 
                 
   //signal declarations 
   state_type state_reg, state_next; 
 
+  // clock and reset 
   always_ff @(posedge clk, posedge reset)
     begin 
       if(reset)
@@ -25,6 +27,7 @@ module delayed_debouncer (
         state_reg <= state_next;
     end
 
+  // next state logic
   always_comb
     begin 
       case(state_reg)
@@ -33,6 +36,7 @@ module delayed_debouncer (
             state_next = wait1_0;
           else
             state_next = zero;
+        
         wait1_0:
           if(sw)
             if(m_tick)
@@ -42,29 +46,66 @@ module delayed_debouncer (
           else 
             state_next = zero ;
         
-         wait1_1:
+        wait1_1:
           if(sw)
             if(m_tick)
-              state_next = wait1_1;
+              state_next = wait1_2;
             else
-              state_next = wait1_0;
+              state_next = wait1_1;
           else 
             state_next = zero ;
         
-         wait1_2:
+        wait1_2:
           if(sw)
             if(m_tick)
-              state_next = wait1_1;
+              state_next = one;
             else
-              state_next = wait1_0;
+              state_next = wait1_2;
           else 
             state_next = zero ;
-        
+
+        one:
+          if(~sw)
+            state_next = wait0_0 ; 
+          else
+            state_next = one;
+
+        wait0_0:
+          if(~sw)
+            if(m_tick)
+              state_next = wait0_1;
+            else
+              state_next = wait0_0;
+          else 
+            state_next = one ;
+
+        wait0_1:
+          if(~sw)
+            if(m_tick)
+              state_next = wait0_2;
+            else
+              state_next = wait0_1;
+          else 
+            state_next = one ;
+
+        wait0_2:
+          if(~sw)
+            if(m_tick)
+              state_next = zero;
+            else
+              state_next = wait0_2;
+          else 
+            state_next = one ;
+        default: state_next = zero;
+      endcase    
     end
-  
-  
-                
-      
+
+  // moore output
+  assign db = ( (state_next = one) ||   
+                 (state_next = wait0_0) || 
+                 (state_next = wait0_1) || 
+                 (state_next = wait0_2) 
+              );    
     end
                 
     
