4ac6ebbfc724ada2100477e968c75ad575f5a3b7
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-06-25 14:46:59 +0530

    Update logic.v

--- TDPR/logic.v
+++ TDPR/logic.v
@@ -5,7 +5,12 @@
 module True_DPR ( clk , en_a , en_b , we_a, we_b , din_a , din_b, dout_a, dout_b, addr_a, addr_b );
 
   #parameter ADDR_SIZE = 8 ;
-  #parameter RAM_WIDTH = 8 ;
+  #parameter DATA_SIZE = 8 ;
   #parameter RAM_SIZE = 1 << ADDR_WIDTH ;
+
+  input clk, en_a , en_b , we_a , we_b ; 
+  input [ DATA_SIZE - 1 : 0 ] din_a, din_b;
+  input [ ADDR_SIZE - 1 : 0 ] addr_a , addr_b ;
+  output reg [ DATA_SIZE - 1 : 0 ] dout_a, dout_b ;
   
   
