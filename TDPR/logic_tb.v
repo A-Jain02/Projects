83170a9c5626f3ae613288eae1ed0c98dbd8887e
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-06-25 15:18:45 +0530

    Update logic_tb.v

--- TDPR/logic_tb.v
+++ TDPR/logic_tb.v
@@ -2,12 +2,16 @@
 
 module True_DPR_tb;
 
+  parameter ADDR_SIZE = 8 ;
+  parameter DATA_SIZE = 8 ;
+  parameter RAM_SIZE = 1 << ADDR_WIDTH ;
+
   reg clk, en_a , en_b , we_a , we_b ; 
   reg [ DATA_SIZE - 1 : 0 ] din_a, din_b;
   reg [ ADDR_SIZE - 1 : 0 ] addr_a , addr_b ;
   wire [ DATA_SIZE - 1 : 0 ] dout_a, dout_b ;
 
-  module True_DPR dut (
+  module True_DPR dut #( .ADDR_SIZE(ADDR_SIZE), .DATA_SIZE(DATA_SIZE), .RAM_SIZE(RAM_SIZE) (
     .clk (clk) ,
     .en_a (en_a),
     .en_b (en_b),
