13955c419fb5c80ad34c1147035dd752d2c8c9ff
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-06-25 14:28:31 +0530

    Update logic.v

--- TDPR/logic.v
+++ TDPR/logic.v
@@ -1,5 +1,6 @@
-`timescale 10ns/10ps
+`timescale 1ns/1ps
 
 
 
-module True_DPR ( clk , en_a , en_b ,
+module True_DPR ( clk , en_a , en_b , we_a, we_b , din_a , din_b, dout_a, dout_b, addr_a, addr_b )
+  
