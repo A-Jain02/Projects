2f771f954ee7c854948794fe12d5f3be2ebbc020
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-07-04 16:54:40 +0530

    Update main.v

--- UART/main.v
+++ UART/main.v
@@ -5,7 +5,7 @@ module UART
             SB_TICK = 16; // stop bits 
 )
 (
-  input clk , rst_n ;
+  input clk , reset_n ;
 
   // receiver ports
   output [ DBIT - 1 : 0 ] rx_data; 
@@ -20,7 +20,73 @@ module UART
   output tx;
 
   // baud clk generator
-  input [10:0] FINAL_VALUE
+  input [10:0] TIMER_FINAL_VALUE;
+
+);
+
+  //timer as baug rate generator
+
+  wire tick;
+  timer_input #( .BITS(11)) baud_rate_generator (
+    .clk(clk),
+    .reset_n(reset_n),
+    .enable(1'b1),
+    .FINAL_VALUE(TIMER_FINAL_VALUE),
+    .done(tick)
+  );
+
+  //receiver module 
+
+  wire rx_done_tick;
+  wire [ DBIT - 1 : 0 ] rx_dout;
+  uart_rx #( .DBIT(DBIT), .SB_TICK(SB_TICK)) receiver ( 
+    .clk(clk),
+    .reset_n(reset_n),
+    .rx(rx),
+    .s_tick(tick),
+    .rx_done_tick(rx_done_tick),
+    .rx_dout(rx_dout)
+  );
+
+  fifo_generator_0 rx_FIFO( 
+    .clk(clk),
+    .srst(~reset_n),
+    .din(rx_dout),
+    .wr_en(rx_done_tick),
+    .full(),
+    .dout(rx_data),
+    .rd_en(rx_uart),
+    .empty(rx_empty)
+  );
+
+
+  //transmitter module 
+  wire [DBIT - 1 : 0 ]tx_din ;
+  wire tx_fifo_empty , tx_done_tick ;
+  uart_tx #( .DBIT(DBIT), .SB_TICK(SB_TICK)) transmitter ( 
+    .clk(clk),
+    .reset_n(reset_n),
+    .tx(tx),
+    .s_tick(tick),
+    .tx_start(~tx_fifo_empty),
+    .tx_din(dout),
+    tx_done_tick(rd_en)
+  );
+    
+  
+  fifo_generator_0 tx_FIFO(
+    .clk(clk),
+    .srst(~reset_n),
+    .din(w_data),
+    .wr_en(wr_uart),
+    .full(tx_full),
+    .dout(tx_din),
+    .rd_en(tx_done_tick),
+    .empty(tx_fifo_empty)
+  );
+
+endmodule
+    
   
 
 
