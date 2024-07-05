0085dcf22424fcfa21ce801a6815a192cfde8dc8
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-07-05 16:52:24 +0530

    Update uart_tb.v

--- UART/uart_tb.v
+++ UART/uart_tb.v
@@ -14,6 +14,31 @@ module uart_tb () ;
   wire tx_done_tick;
   wire tx;
 
+
+  // Parameters
+    parameter CLOCK_FREQ = 100000000;  // 50 MHz system clock
+    parameter BAUD_RATE = 9600;     // Desired baud rate
+
+  // Instantiate UART Transmitter
+    uart_tx #(.CLOCK_FREQ(CLOCK_FREQ), .BAUD_RATE(BAUD_RATE)) uart_tx_inst (
+        .clk(clk),
+        .rst(rst),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+        .tx(tx),
+      .tx_done_tick(tx_done_tick)
+    );
+
+   // Instantiate UART Receiver
+    uart_rx #(.CLOCK_FREQ(CLOCK_FREQ), .BAUD_RATE(BAUD_RATE)) uart_rx_inst (
+        .clk(clk),
+        .rst(rst),
+        .rx(tx),  // Connect transmitter output to receiver input
+        .rx_data(rx_data),
+      .rx_done_tick(rx_done_tick)
+    );
+
+  
   
   
 
