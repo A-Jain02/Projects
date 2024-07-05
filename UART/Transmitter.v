bc11238e71c4cabb82ab8570712ae92245204046
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-07-05 16:25:40 +0530

    Update Transmitter.v

--- UART/Transmitter.v
+++ UART/Transmitter.v
@@ -5,7 +5,7 @@ module uart_tx #( parameter DBIT = 8 , SB_TICK = 16)
     input clk , reset_n , tx_start,
     input [ DBIT - 1 : 0 ] tx_din ,
     output tx_done_tick ,
-    output tx 
+    output reg tx 
   );
 
   
