63bd8326ae35a041902f5b45801452d17a57cad5
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-07-05 16:49:35 +0530

    Update uart_tb.v

--- UART/uart_tb.v
+++ UART/uart_tb.v
@@ -5,6 +5,20 @@ module uart_tb () ;
 `include " uart_tx.v"
 `include " uart_rx.v"
 
+  reg clk ;
+  reg reset_n ;
+  reg tx_start ; 
+  reg [ DBIT - 1 : 0 ] tx_data ;
+  wire [ DBIT - 1 : 0 ] rx_data ;
+  wire rx_done_tick;
+  wire tx_done_tick;
+  wire tx;
+
+  
+  
+
+
+
   
 
   
