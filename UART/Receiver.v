1d1e94572ae7f385d446455cc63bcaf25165d97e
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-07-05 16:35:16 +0530

    Update Receiver.v

--- UART/Receiver.v
+++ UART/Receiver.v
@@ -15,7 +15,7 @@ module uart_rx #( parameter DBIT = 8 , SB_TICK = 16)
     reg [ 1 : 0 ] state ; // the 4 states
 
     wire s_tick;
-    timer_input #( .BITS(11)) baud_rate_generator (
+    timer_input #( .BITS(10)) baud_rate_generator (
     .clk(clk),
     .reset_n(reset_n),
     .done(s_tick)
