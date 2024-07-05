f3d79c1a71e1ea56db57976c838be46f3215d8c4
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2024-07-05 16:19:50 +0530

    Update Transmitter.v

--- UART/Transmitter.v
+++ UART/Transmitter.v
@@ -8,13 +8,15 @@ module uart_tx #( parameter DBIT = 8 , SB_TICK = 16)
     output tx 
   );
 
+  
+
    localparam idle = 0 , start = 1 , data = 2 , stop = 3;
   
-    reg [ 2 : 0 ] n_reg , n_next;  // counter for DBITS
-    reg [ 3 : 0 ] s_reg , s_next;  // counter for s_tick
-    reg [ DBIT - 1 : 0 ] b_reg , b_next ; // stores the received data bits 
-    reg [ 1 : 0 ] state , state_next ; // the 4 states
-    reg tx , tx_next ; // tracking the transmitted bit
+    reg [ 2 : 0 ] n_reg ;  // counter for DBITS
+    reg [ 3 : 0 ] s_reg ;  // counter for s_tick
+    reg [ DBIT - 1 : 0 ] b_reg  ; // stores the received data bits 
+    reg [ 1 : 0 ] state  ; // the 4 states
+    reg tx_next ; // tracking the transmitted bit
 
     // state and reg initialization
     always @(posedge clk)
@@ -25,80 +27,65 @@ module uart_tx #( parameter DBIT = 8 , SB_TICK = 16)
             n_reg <= 0;
             b_reg <= 0;
           end 
-          else begin 
-            state <= state_next ; 
-            s_reg <= s_next ;
-            n_reg <= n_next ;
-            b_reg <= b_next ;
-          end
-      end
-
-  always @*
-    begin
-        state_next <= state ;
-        s_next <= s_reg ;
-        n_next <= n_reg ;
-        b_next <= b_reg ; 
-        tx_done_tick = 1'b0;
-
-    case (state) 
-      idle:
-        begin
-          tx_next = 1'b1;
-          if(tx_start)
-            begin
-              s_next = 0;
-              b_next = tx_din;
-              state_next = start ;
-            end
-        end
-  
-      start : 
-        begin
-          tx_next = 1'b0;
-          if(s_tick)
-              if(s_reg == 15) 
-                begin 
-                  s_next = 0;
-                  n_next = 0;
-                  state_next = data ;
-                end
-            else 
-              s_next = s_reg + 1;
-        end
-  
-      data : 
-        begin 
-          tx_next = b_reg[0] ; 
-          if(s_tick)
-              if(s_reg == 15)
-                begin
-                  s_next = 0;
-                  b_next = { 1'b0 , b_reg[ DBIT -1 : 1 ] };
-                  if( n_reg == (DBIT - 1))
-                    state_next = stop;
-                else
-                  n_next = n_reg + 1;
-                end
-              else
-                s_next = s_reg + 1;
-        end
-  
-      stop : 
-        begin
-          tx_next = 1'b1;
-          if(s_tick)
-            if(s_next == (SB_TICK - 1)) begin
-                tx_done_tick = 1;
-                state_next = idle;
-              end
-            else
-              s_next = s_reg + 1;
-        end
-  
-      default : state_next = idle ;
-  
-    endcase 
+          else begin
+            tx_done_tick = 1'b0;
+            case (state) 
+                  idle:
+                    begin
+                      tx_next = 1'b1;
+                      if(tx_start)
+                        begin
+                          s_reg = 0;
+                          b_reg = tx_din;
+                          state_reg = start ;
+                        end
+                    end
+              
+                  start : 
+                    begin
+                      tx_next = 1'b0;
+                      if(s_tick)
+                          if(s_reg == 15) 
+                            begin 
+                              s_reg = 0;
+                              n_reg = 0;
+                              state_reg = data ;
+                            end
+                        else 
+                          s_reg = s_reg + 1;
+                    end
+              
+                  data : 
+                    begin 
+                      tx_next = b_reg[0] ; 
+                      if(s_tick)
+                          if(s_reg == 15)
+                            begin
+                              s_reg = 0;
+                              b_reg = { 1'b0 , b_reg[ DBIT -1 : 1 ] };
+                              if( n_reg == (DBIT - 1))
+                                state_reg = stop;
+                            else
+                              n_reg = n_reg + 1;
+                            end
+                          else
+                            s_reg = s_reg + 1;
+                    end
+              
+                  stop : 
+                    begin
+                      tx_next = 1'b1;
+                      if(s_tick)
+                        if(s_reg == (SB_TICK - 1)) begin
+                            tx_done_tick = 1;
+                            state_reg = idle;
+                          end
+                        else
+                          s_reg = s_reg + 1;
+                    end
+              
+                  default : state_reg = idle ;
+            endcase 
   end
 endmodule
       
